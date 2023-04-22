71b409e057d86a936d39a11fdda3b2d0865b1ab1
bsw98 <qwqq6954@naver.com>
2023-04-22 23:48:11 +0900

    fix: 파이어베이스 env파일 연동오류 수정(#3)

--- src/firebase/app.ts
+++ src/firebase/app.ts
@@ -1,6 +1,4 @@
 import firebase from "firebase/compat/app";
-import { getStorage, uploadBytes, ref } from "firebase/storage";
-import { collection, getDocs } from "firebase/firestore";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
@@ -8,32 +6,13 @@ import "firebase/compat/database";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWOla15HC63D07Sw1zkrvwofQtJf45Vx8",
-  authDomain: "petstagram-fe291.firebaseapp.com",
-  projectId: "petstagram-fe291",
-  storageBucket: "petstagram-fe291.appspot.com",
-  messagingSenderId: "440336677151",
-  appId: "1:440336677151:web:8f8d8e93cf46d582cb582a",
-  measurementId: "G-ZBMHJB9CVD",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
-
-// const {
-//   VITE_API_KEY,
-//   VITE_AUTH_DOMAIN,
-//   VITE_PROJECT_ID,
-//   VITE_MESSAGE_SENDER_ID,
-//   VITE_STORAGE_BUCKET,
-//   VITE_APP_ID,
-// } = process.env;
-
-// const firebaseConfig = {
-//   apiKey: VITE_API_KEY,
-//   authDomain: VITE_AUTH_DOMAIN,
-//   projectId: VITE_PROJECT_ID,
-//   storageBucket: VITE_STORAGE_BUCKET,
-//   messagingSenderId: VITE_MESSAGE_SENDER_ID,
-//   appId: VITE_APP_ID,
-// };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -46,6 +25,6 @@ export const db = firebase.firestore();
 export const storage = firebase.storage();
 
 export const usersRef = db.collection("users"); // 유저 정보 컬렉션
-export const userWriteRef = db.collection("UserWrite"); // 게시글 정보 컬렉션
+export const userWriteRef = db.collection("posts"); // 게시글 정보 컬렉션
 
 export default firebase;
