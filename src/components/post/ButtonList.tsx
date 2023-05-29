ef401fb6713281727fa653ff47a54e2037ec9458
bsw98 <qwqq6954@naver.com>
2023-05-30 01:17:14 +0900

    feat: props 받아서 재활용 가능하게 완성 (#59)

--- src/components/post/ButtonList.tsx
+++ src/components/post/ButtonList.tsx
@@ -1,40 +1,70 @@
+import Link from "next/link";
 import styled from "styled-components";
 
-export default function ButtonList() {
+interface props {
+  isFirst?: boolean;
+  isLast?: boolean;
+  type: "button" | "link";
+  name: string;
+  href?: string;
+}
+type StyledProps = {
+  isFirst?: boolean;
+  isLast?: boolean;
+};
+export default function ExtraInfoModal({
+  isFirst,
+  isLast,
+  type,
+  name,
+  href,
+}: props) {
   return (
-    <Div>
-      <button>삭제</button>
-      <button>수정</button>
-      <button>좋아요 수 숨기기</button>
-      <button>댓글 기능 해제</button>
-      <a>게시물로 이동</a>
-      <button>취소</button>
-    </Div>
+    <>
+      {type == "button" ? (
+        <ExtraInfoModalButton isFirst={isFirst} isLast={isLast}>
+          {name}
+        </ExtraInfoModalButton>
+      ) : (
+        <ExtraInfoModalLink isFirst={isFirst} isLast={isLast} href={href}>
+          {name}
+        </ExtraInfoModalLink>
+      )}
+    </>
   );
 }
 
-const Div = styled.div`
-  * {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    width: 467px;
-    height: 80px;
-    background: #ffb800;
-    border: 0;
-    cursor: pointer;
-    border-bottom: 1px solid #ffffff;
-    color: #ffffff;
-    font-weight: 600;
-    font-size: 20px;
-  }
-  *:first-child {
-    border-radius: 12px 12px 0px 0px;
-  }
-  *:last-child {
-    border-radius: 0 0 12px 12px;
-  }
-  a {
-  }
+const ExtraInfoModalButton = styled.button<StyledProps>`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 467px;
+  height: 80px;
+  background: #ffb800;
+  border: 0;
+  cursor: pointer;
+  border-bottom: 1px solid #ffffff;
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 20px;
+  border-radius: ${({ isFirst, isLast }) =>
+    isFirst ? "12px 12px 0 0" : isLast ? "0 0 12px 12px" : "0"};
+`;
+const ExtraInfoModalLink = styled.a<StyledProps>`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 467px;
+  height: 80px;
+  background: #ffb800;
+  border: 0;
+  cursor: pointer;
+  border-bottom: 1px solid #ffffff;
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 20px;
+  border-radius: ${({ isFirst, isLast }) =>
+    isFirst ? "12px 12px 0 0" : isLast ? "0 0 12px 12px" : "0"};
 `;
