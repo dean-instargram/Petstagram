856c254efd327249b40a361bb26158aa27641781
djswns7 <duftlagltkfwk@gmail.com>
2023-06-28 12:43:57 +0900

    fix: 전역스타일에 'box-sizing:border-box' 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import { Provider } from 'react-redux';
-import { AppProps } from 'next/app';
-import store from '../redux/store';
-import { createGlobalStyle } from 'styled-components';
-import normalize from 'styled-normalize';
-import reset from 'styled-reset';
-import LoginCheck from '@/components/loginCheck';
-import Navigate from '@/components/Navigate';
-import { useEffect, useState } from 'react';
-import { getUserUid } from '@/firebase/utils';
+import { Provider } from "react-redux";
+import { AppProps } from "next/app";
+import store from "../redux/store";
+import { createGlobalStyle } from "styled-components";
+import normalize from "styled-normalize";
+import reset from "styled-reset";
+import LoginCheck from "@/components/loginCheck";
+import Navigate from "@/components/Navigate";
+import { useEffect, useState } from "react";
+import { getUserUid } from "@/firebase/utils";
 
-import type { ReactElement, ReactNode } from 'react';
-import type { NextPage } from 'next';
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
 
 // 각 페이지에서 불러와서 쓸 '레이아웃 적용된 페이지의 type'
 type NextPageWithLayout = NextPage & {
@@ -52,6 +52,7 @@ const GlobalStyles = createGlobalStyle`
    ${normalize}
    body {
     font-family: 'Pretendard', sans-serif;
+    box-sizing:border-box;
   }
   .a11y-hidden {
   position: absolute;
